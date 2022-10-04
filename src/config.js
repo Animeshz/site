8cc24d20261dee1fbd22a322a0767324439dc45d
Animesh Sahu <animeshsahu19@yahoo.com>
2022-10-05 04:29:32 +0530

    Add RSS logo on navbar

--- src/config.js
+++ src/config.js
@@ -38,13 +38,13 @@ const config = {
     outlineTitle: 'Table of Contents',
     outline: [2, 3],
     socialLinks: [
+      { icon: { svg: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-rss" viewBox="0 0 16 16"><path d="M5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-3-8.5a1 1 0 0 1 1-1c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0 8 8 0 0 0-8-8 1 1 0 0 1-1-1zm0 4a1 1 0 0 1 1-1 6 6 0 0 1 6 6 1 1 0 1 1-2 0 4 4 0 0 0-4-4 1 1 0 0 1-1-1z"/></svg>' }, link: '/site/atom.xml' },
       { icon: 'github', link: 'https://github.com/Animeshz/site' },
       // Better on footer
       // { icon: 'twitter', link: '...' },
       // { icon: 'linkedin', link: '...' },
       // { icon: 'reddit', link: '...' },
       // { icon: 'patreon', link: '...' },
-      // { icon: 'rss', link: '...' },
     ],
     editLink: { pattern: 'https://github.com/Animeshz/site/edit/main/content/:path' },
     nav: [
