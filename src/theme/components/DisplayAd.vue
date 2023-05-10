4dd4693a7829b44955d002fda83d72a90bc06e31
Animesh Sahu <animeshsahu19@yahoo.com>
2023-05-10 18:52:43 +0530

    Remove any advertisement related things

--- src/theme/components/DisplayAd.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3494660420162247" data-ad-slot="9836075224"
-        data-ad-format="auto" data-full-width-responsive="true"></ins>
-</template>
--- src/theme/components/MultiplexAd.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <ins class="adsbygoogle" style="display:block" data-ad-format="autorelaxed" data-ad-client="ca-pub-3494660420162247"
-        data-ad-slot="4645374013"></ins>
-</template>
